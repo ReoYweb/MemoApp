629d3611a0be7f069aaaffc6f203bf820274a07c
吉岡怜央 <yoshiokareiou@yoshiokiounoMBP.hitronhub.home>
2019-04-22 16:51:22 -0700

    put styles to appber

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import BodyText from "./src/elements/BodyText";
+// import BodyText from "./src/elements/BodyText";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
-        <BodyText>Hi!</BodyText>
+        <View style={styles.appber}>
+          <View>
+            <Text style={styles.appberTitle}>MEMOT</Text>
+          </View>
+        </View>
+
+        {/* <View style={styles.memoList}>
+          <View style={style.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2019.2.3</Text>
+          </View>
+        </View>
+
+        <View>+</View> */}
       </View>
     );
   }
@@ -19,5 +31,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  appber: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#000",
+    width: "100%",
+    height: 100,
+    paddingTop: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  appberTitle: {
+    color: "white",
+    fontSize: 18
   }
 });
