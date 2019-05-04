539525f067a8b57ee75ac68b64a52e0129406aa5
吉岡怜央 <yoshioka.web.d@gmail.com>
2019-05-04 13:20:51 -0700

    create text input

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 import AddButton from "../elements/AddButton";
 
 class MemoEditScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.memoEditInput}>
-          <Text>Hi!</Text>
-        </View>
+        <TextInput
         <AddButton>{"\uf00c"}</AddButton>
       </View>
     );
@@ -19,6 +17,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%"
+  },
+  memoEditInput: {
+    backgroundColor: "#fff",
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16
   }
 });
 
